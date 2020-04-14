b3ce84bef6a4bf8f05cb125069356ff68ae790b0
MZ-Dino <38118953+Dino-MZ@users.noreply.github.com>
2020-04-13 22:43:22 -0500

    Add files via upload
    I was playing around with some C# code. nothing interesting, just a test...

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NewProgram
+{
+	class Program{
+		static void Main(string[] args){
+			private string myString = "Hello World";
+			Console.WriteLine("Hello World");
+		}
+	}
+}
